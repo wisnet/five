eea6c0113af9f94bda75700fd031293630fdbe3b
Michael Dahlke <madahlke27@gmail.com>
2019-03-01 16:03:32 -0600

    init

--- node_modules/@wordpress/dom/src/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Internal dependencies
- */
-import * as focusable from './focusable';
-import * as tabbable from './tabbable';
-
-export const focus = { focusable, tabbable };
-
-export * from './dom';

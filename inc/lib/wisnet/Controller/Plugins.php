f946e815cc155835379e269326a37ba6f94d2bd1
Michael Dahlke <madahlke27@gmail.com>
2019-03-13 14:55:49 -0500

    move bundled plugins to github

--- inc/lib/wisnet/Controller/Plugins.php
+++ inc/lib/wisnet/Controller/Plugins.php
@@ -63,15 +63,16 @@ class Plugins {
 				'name' => 'Relevanssi',
 				'slug' => 'relevanssi',
 				'required' => false,
+				'source' => 'https://github.com/wisnet/five-plugins/raw/master/relevanssi.zip',
 			],
 			[
 				'name' => 'The Events Calendar',
-				'slug' => 'the-events-calendar',
+				'slug' => 'the - events - calendar',
 				'required' => false,
 			],
 			[
 				'name' => 'Timber',
-				'slug' => 'timber-library',
+				'slug' => 'timber - library',
 				'required' => true,
 			],
 			[
@@ -119,83 +120,6 @@ class Plugins {
 			'dismiss_msg' => '',                      // If 'dismissable' is false, this message will be output at top of nag.
 			'is_automatic' => true,                   // Automatically activate plugins after installation or not.
 			'message' => '',                      // Message to output right before the plugins table.
-
-			/*
-			'strings'      => array(
-				'page_title'                      => __( 'Install Required Plugins', 'wisnet' ),
-				'menu_title'                      => __( 'Install Plugins', 'wisnet' ),
-				/* translators: %s: plugin name. * /
-				'installing'                      => __( 'Installing Plugin: %s', 'wisnet' ),
-				/* translators: %s: plugin name. * /
-				'updating'                        => __( 'Updating Plugin: %s', 'wisnet' ),
-				'oops'                            => __( 'Something went wrong with the plugin API.', 'wisnet' ),
-				'notice_can_install_required'     => _n_noop(
-					/* translators: 1: plugin name(s). * /
-					'This theme requires the following plugin: %1$s.',
-					'This theme requires the following plugins: %1$s.',
-					'wisnet'
-				),
-				'notice_can_install_recommended'  => _n_noop(
-					/* translators: 1: plugin name(s). * /
-					'This theme recommends the following plugin: %1$s.',
-					'This theme recommends the following plugins: %1$s.',
-					'wisnet'
-				),
-				'notice_ask_to_update'            => _n_noop(
-					/* translators: 1: plugin name(s). * /
-					'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-					'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-					'wisnet'
-				),
-				'notice_ask_to_update_maybe'      => _n_noop(
-					/* translators: 1: plugin name(s). * /
-					'There is an update available for: %1$s.',
-					'There are updates available for the following plugins: %1$s.',
-					'wisnet'
-				),
-				'notice_can_activate_required'    => _n_noop(
-					/* translators: 1: plugin name(s). * /
-					'The following required plugin is currently inactive: %1$s.',
-					'The following required plugins are currently inactive: %1$s.',
-					'wisnet'
-				),
-				'notice_can_activate_recommended' => _n_noop(
-					/* translators: 1: plugin name(s). * /
-					'The following recommended plugin is currently inactive: %1$s.',
-					'The following recommended plugins are currently inactive: %1$s.',
-					'wisnet'
-				),
-				'install_link'                    => _n_noop(
-					'Begin installing plugin',
-					'Begin installing plugins',
-					'wisnet'
-				),
-				'update_link' 					  => _n_noop(
-					'Begin updating plugin',
-					'Begin updating plugins',
-					'wisnet'
-				),
-				'activate_link'                   => _n_noop(
-					'Begin activating plugin',
-					'Begin activating plugins',
-					'wisnet'
-				),
-				'return'                          => __( 'Return to Required Plugins Installer', 'wisnet' ),
-				'plugin_activated'                => __( 'Plugin activated successfully.', 'wisnet' ),
-				'activated_successfully'          => __( 'The following plugin was activated successfully:', 'wisnet' ),
-				/* translators: 1: plugin name. * /
-				'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'wisnet' ),
-				/* translators: 1: plugin name. * /
-				'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'wisnet' ),
-				/* translators: 1: dashboard link. * /
-				'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'wisnet' ),
-				'dismiss'                         => __( 'Dismiss this notice', 'wisnet' ),
-				'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'wisnet' ),
-				'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'wisnet' ),
-
-				'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
-			),
-			*/
 		];
 
 		tgmpa($plugins, $config);

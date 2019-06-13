17c807148da6bff9bb15c0fdaa5addb96e3e3203
Michael Dahlke <mdahlke@wisnet.com>
2019-06-13 10:57:49 -0500

    comment out menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,16 +1,17 @@
-import $ from 'jquery';
+// import $ from 'jquery';
+//
+// $('.dropdown-menu a.dropdown-toggle').on('click', function () {
+// 	if (!$(this).next().hasClass('show')) {
+// 		$(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+// 	}
+// 	const $subMenu = $(this).next('.dropdown-menu');
+// 	$subMenu.toggleClass('show');
+//
+//
+// 	$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function () {
+// 		$('.dropdown-submenu .show').removeClass('show');
+// 	});
+//
+// 	return false;
+// });
 
-$('.dropdown-menu a.dropdown-toggle').on('click', function () {
-	if (!$(this).next().hasClass('show')) {
-		$(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
-	}
-	const $subMenu = $(this).next('.dropdown-menu');
-	$subMenu.toggleClass('show');
-	
-	
-	$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function () {
-		$('.dropdown-submenu .show').removeClass('show');
-	});
-	
-	return false;
-});

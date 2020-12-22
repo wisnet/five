e0e0ea4cf875fb2a8e98a885d993671223a71fa9
Michael Dahlke <mdahlke@wisnet.com>
2020-12-22 08:24:35 -0600

    abstract registration of custom post types and taxonomies for easier creation

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -8,47 +8,53 @@
  * @author Michael Dahlke <mdahlke@wisnet.com>
  */
 
-function five_register_my_cpts() {
-	/**
-	 * Post Type: Team Members.
-	 */
+add_action('init', 'five_register_my_cpts');
 
-	$labels = [
-		'name' => __('Team Members', 'custom-post-type-ui'),
-		'singular_name' => __('Team Member', 'custom-post-type-ui'),
-		'edit_item' => __('Edit Team Member', 'custom-post-type-ui'),
-		'new_item' => __('New Team Member', 'custom-post-type-ui'),
-		'view_item' => __('View Team Member', 'custom-post-type-ui'),
-		'view_items' => __('View Team Members', 'custom-post-type-ui'),
-		'all_items' => __('All Team Members', 'custom-post-type-ui'),
-		'add_new_item' => __('Add Team Member', 'custom-post-type-ui'),
-		'add_new' => __('Add Team Member', 'custom-post-type-ui'),
-	];
-
-	$args = [
-		'label' => __('Team Members', 'custom-post-type-ui'),
-		'labels' => $labels,
-		'description' => '',
-		'public' => true,
-		'publicly_queryable' => true,
-		'show_ui' => true,
-		'delete_with_user' => false,
-		'show_in_rest' => true,
-		'rest_base' => '',
-		'rest_controller_class' => 'WP_REST_Posts_Controller',
-		'has_archive' => true,
-		'show_in_menu' => true,
-		'show_in_nav_menus' => true,
-		'exclude_from_search' => false,
-		'capability_type' => 'post',
-		'map_meta_cap' => true,
-		'hierarchical' => false,
-		'rewrite' => ['slug' => 'team', 'with_front' => true],
-		'query_var' => true,
-		'supports' => ['title', 'editor', 'thumbnail', 'comments'],
-	];
-
-	register_post_type('team_member', $args);
+function five_register_my_cpts()
+{
+    /**
+     * Team Members
+     */
+    five_register_post_type('team-members', 'Team Member', 'Team Members');
 }
 
-add_action('init', 'five_register_my_cpts');
+function five_register_post_type($postType, $singular, $plural, $args = [])
+{
+    $labels = [
+        'name' => __($plural, 'custom-post-type-ui'),
+        'singular_name' => __($singular, 'custom-post-type-ui'),
+        'edit_item' => __('Edit '.$singular, 'custom-post-type-ui'),
+        'new_item' => __('New '.$singular, 'custom-post-type-ui'),
+        'view_item' => __('View '.$singular, 'custom-post-type-ui'),
+        'view_items' => __('View '.$singular, 'custom-post-type-ui'),
+        'all_items' => __('All '.$plural, 'custom-post-type-ui'),
+        'add_new_item' => __('Add '.$singular, 'custom-post-type-ui'),
+        'add_new' => __('Add '.$singular, 'custom-post-type-ui'),
+    ];
+
+    $args = array_merge([
+        'label' => __($plural, 'five'),
+        'description' => '',
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'delete_with_user' => false,
+        'show_in_rest' => true,
+        'rest_base' => '',
+        'rest_controller_class' => 'WP_REST_Posts_Controller',
+        'has_archive' => true,
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'exclude_from_search' => false,
+        'capability_type' => 'post',
+        'map_meta_cap' => true,
+        'hierarchical' => false,
+        'rewrite' => ['slug' => sanitize_title($plural), 'with_front' => true],
+        'query_var' => true,
+        'supports' => ['title', 'editor', 'thumbnail', 'comments', 'page-attributes'],
+    ], $args);
+
+    $args['labels'] = $labels;
+
+    register_post_type($postType, $args);
+}
--- inc/custom-taxonomies.php
+++ inc/custom-taxonomies.php
@@ -7,3 +7,45 @@
  * @package wisnet Five
  * @author Michael Dahlke <mdahlke@wisnet.com>
  */
+//hook into the init action and call create_book_taxonomies when it fires
+add_action('init', 'five_register_my_taxonomies', 0);
+
+//create a custom taxonomy name it topics for your posts
+
+function five_register_my_taxonomies()
+{
+    //five_register_taxonomy('custom-taxonomy', 'Custom Taxonomy', 'Custom Taxonomies', ['custom-post-type']);
+}
+
+function five_register_taxonomy($taxonomy, $singular, $plural, $object_type = ['post'], $args = [])
+{
+    // Add new taxonomy, make it hierarchical like categories
+    //first do the translations part for GUI
+
+    $labels = [
+        'name' => _x($plural, 'five'),
+        'singular_name' => _x($singular, 'five'),
+        'search_items' => __('Search '.$plural),
+        'all_items' => __('All '.$plural),
+        'parent_item' => __('Parent '.$singular),
+        'parent_item_colon' => __('Parent '.$singular.':'),
+        'edit_item' => __('Edit '.$singular),
+        'update_item' => __('Update '.$singular),
+        'add_new_item' => __('Add New '.$singular),
+        'new_item_name' => __('New '.$singular.' Name'),
+        'menu_name' => __($plural),
+    ];
+
+    // Now register the taxonomy
+    $args = array_merge([
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_in_rest' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => ['slug' => $taxonomy],
+    ], $args);
+
+    register_taxonomy($taxonomy, $object_type, $args);
+}
